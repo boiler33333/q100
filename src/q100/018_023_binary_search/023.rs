073a1a098e8454ca89a6ae89be6726204dae133b
boiler <km.angstr3mp@gmail.com>
2021-09-07 08:55:23 +0900

    update

--- src/q100/018_023_binary_search/023.rs
+++ src/q100/018_023_binary_search/023.rs
@@ -24,26 +24,47 @@ impl<T: Ord> BinarySearch<T> for [T] {
 
 fn main() {
   input! {
-    n: usize,
-    m: usize,
-    mut p: [usize; n],
+    n: usize, m: usize,
+    p: [usize; n],
   }
+  let ans = solve(m, &p);
+  println!("{}", ans);
+}
+
+fn solve(m: usize, p: &[usize]) -> usize {
+  let mut p = p.to_vec();
   p.push(0);
-  let mut s = Vec::new();
-  for p1 in &p {
-    for p2 in &p {
-      s.push(p1 + p2);
+  let mut q = Vec::new();
+  for &p1 in &p {
+    for &p2 in &p {
+      q.push(p1 + p2);
     }
   }
-  s.sort();
-  let mut ans = 0;
-  for &a in &s {
-    if m < a {
+  q.sort();
+  let mut point = 0;
+  for &a in &q {
+    if a > m {
       break;
     }
     let b = m - a;
-    let i = s.lower_bound(&b) - 1;
-    ans = max(a + s[i], ans);
+    let i = q.lower_bound(&b);
+    point = max(point, a + q[i-1]);
   }
-  println!("{}", ans);
+  point
+}
+
+#[test]
+fn test_solve_1() {
+  let m = 50;
+  let p = vec![3, 14, 15, 9];
+  let result = solve(m, &p);
+  assert_eq!(result, 48);
+}
+
+#[test]
+fn test_solve_2() {
+  let m = 21;
+  let p = vec![16, 11, 2];
+  let result = solve(m, &p);
+  assert_eq!(result, 20);
 }
