fd9d729993c01804f7021af9deada0453039e1f5
boiler <km.angstr3mp@gmail.com>
2021-09-11 07:44:46 +0900

    update

--- src/q100/028_033_breadth_first_search/031.rs
+++ src/q100/028_033_breadth_first_search/031.rs
@@ -1,64 +1,68 @@
 use proconio::input;
 use std::collections::VecDeque;
 
-fn bfs(h: usize, w: usize, g: &Vec<Vec<usize>>) -> usize {
+fn bfs(
+  h: usize,
+  w: usize,
+  building: &Vec<Vec<usize>>,
+  sy: usize,
+  sx: usize,
+) -> usize {
   let mut cnt = 0;
-  let mut que: VecDeque<(usize, usize)> = VecDeque::new();
   let mut seen = vec![vec![false; w]; h];
-  que.push_back((0, 0));
-  seen[0][0] = true;
-  while let Some((x, y)) = que.pop_front() {
-    if y % 2 == 0 {
+  let mut que: VecDeque<(usize, usize)> = VecDeque::new();
+  seen[sy][sx] = true;
+  que.push_back((sx, sy));
+  while let Some((ux, uy)) = que.pop_front() {
+    if uy % 2 == 0 {
       for i in 0..6 {
-        if y == 0 && i <= 1 || y == h-1 && i >= 4 {
+        if uy == 0 && i <= 1 || uy == h-1 && 4 <= i {
           continue;
         }
-        if x == 0 && i % 2 == 0 || x == w-1 && i == 3 {
+        if ux == 0 && i % 2 == 0 || ux == w-1 && i == 3 {
           continue;
         }
-        let (x2, y2) = match i {
-          0 => (x - 1, y - 1),
-          1 => (x + 0, y - 1),
-          2 => (x - 1, y + 0),
-          3 => (x + 1, y + 0),
-          4 => (x - 1, y + 1),
-          _ => (x + 0, y + 1),
+        let (vx, vy) = match i {
+          0 => (ux - 1, uy - 1),
+          1 => (ux + 0, uy - 1),
+          2 => (ux - 1, uy + 0),
+          3 => (ux + 1, uy + 0),
+          4 => (ux - 1, uy + 1),
+          _ => (ux + 0, uy + 1),
         };
-        if seen[y2][x2] {
-          continue;
-        }
-        if g[y2][x2] > 0 {
+        if building[vy][vx] == 1 {
           cnt += 1;
           continue;
         }
-        seen[y2][x2] = true;
-        que.push_back((x2, y2));
+        if !seen[vy][vx] {
+          seen[vy][vx] = true;
+          que.push_back((vx, vy));
+        }
       }
     } else {
       for i in 0..6 {
-        if y == 0 && i <= 1 || y == h-1 && i >= 4 {
+        if uy == 0 && i <= 1 || uy == h-1 && 4 <= i {
           continue;
         }
-        if x == 0 && i == 2 || x == w-1 && i % 2 == 1 {
+        if ux == 0 && i == 2 || ux == w-1 && i % 2 == 1 {
           continue;
         }
-        let (x2, y2) = match i {
-          0 => (x + 0, y - 1),
-          1 => (x + 1, y - 1),
-          2 => (x - 1, y + 0),
-          3 => (x + 1, y + 0),
-          4 => (x + 0, y + 1),
-          _ => (x + 1, y + 1),
+        let (vx, vy) = match i {
+          0 => (ux - 0, uy - 1),
+          1 => (ux + 1, uy - 1),
+          2 => (ux - 1, uy + 0),
+          3 => (ux + 1, uy + 0),
+          4 => (ux - 0, uy + 1),
+          _ => (ux + 1, uy + 1),
         };
-        if seen[y2][x2] {
-          continue;
-        }
-        if g[y2][x2] > 0 {
+        if building[vy][vx] == 1 {
           cnt += 1;
           continue;
         }
-        seen[y2][x2] = true;
-        que.push_back((x2, y2));
+        if !seen[vy][vx] {
+          seen[vy][vx] = true;
+          que.push_back((vx, vy));
+        }
       }
     }
   }
@@ -67,16 +71,18 @@ fn bfs(h: usize, w: usize, g: &Vec<Vec<usize>>) -> usize {
 
 fn main() {
   input! {
-    w: usize,
-    h: usize,
-    mut g: [[usize; w]; h],
+    mut w: usize,
+    mut h: usize,
+    mut table: [[usize; w]; h],
   }
-  for i in 0..h {
-    g[i].insert(0, 0);
-    g[i].push(0);
+  table.insert(0, vec![0; w]);
+  table.push(vec![0; w]);
+  h = table.len();
+  for y in 0..h {
+    table[y].insert(0, 0);
+    table[y].push(0);
   }
-  g.insert(0, vec![0; w+2]);
-  g.push(vec![0; w+2]);
-  let ans = bfs(h+2, w+2, &g);
+  w = table[0].len();
+  let ans = bfs(h, w, &table, 0, 0);
   println!("{}", ans);
 }
