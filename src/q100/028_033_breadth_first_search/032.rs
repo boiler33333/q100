097f993bbb667d153f729acd9c9a8dc6f8d7abab
boiler <km.angstr3mp@gmail.com>
2021-09-11 08:44:45 +0900

    update

--- src/q100/028_033_breadth_first_search/032.rs
+++ src/q100/028_033_breadth_first_search/032.rs
@@ -13,59 +13,49 @@ fn read<T: FromStr>() -> T {
   s.parse().ok().expect("failed parsing")
 }
 
-#[derive(Copy, Clone)]
-struct Wall {
-  right: bool,
-  bottom: bool,
-}
-
-impl Wall {
-  fn new() -> Self {
-    Wall{ right: true, bottom: true }
-  }
-}
-
-
 fn bfs(
   h: usize,
   w: usize,
-  wall: &Vec<Vec<Wall>>,
+  wall: &Vec<Vec<(bool, bool)>>,
+  sy: usize,
+  sx: usize,
+  gy: usize,
+  gx: usize,
 ) -> usize {
   let mut dist = vec![vec![0; w]; h];
   let mut que: VecDeque<(usize, usize)> = VecDeque::new();
-  dist[0][0] = 1;
-  que.push_back((0, 0));
-  while let Some((x, y)) = que.pop_front() {
+  dist[sy][sx] = 1;
+  que.push_back((sx, sy));
+  while let Some((ux, uy)) = que.pop_front() {
     for i in 0..4 {
-      if y == 0 && i == 2 || y == h-1 && i == 0 {
+      if uy == 0 && i == 0 || uy == h-1 && i == 2 {
         continue;
       }
-      if x == 0 && i == 3 || x == w-1 && i == 1 {
+      if ux == 0 && i == 3 || ux == w-1 && i == 1 {
         continue;
       }
-      let (x2, y2) = match i {
-        0 => (x+0, y+1),
-        1 => (x+1, y+0),
-        2 => (x+0, y-1),
-        _ => (x-1, y+0),
-      };
       let is_wall = match i {
-        0 => wall[y][x].bottom,
-        1 => wall[y][x].right,
-        2 => wall[y-1][x].bottom,
-        _ => wall[y][x-1].right,
+        0 => wall[uy-1][ux].1,
+        1 => wall[uy][ux].0,
+        2 => wall[uy][ux].1,
+        _ => wall[uy][ux-1].0,
       };
       if is_wall {
         continue;
       }
-      if dist[y2][x2] > 0 {
-        continue;
+      let (vx, vy) = match i {
+        0 => (ux + 0, uy - 1),
+        1 => (ux + 1, uy + 0),
+        2 => (ux + 0, uy + 1),
+        _ => (ux - 1, uy + 0),
+      };
+      if dist[vy][vx] == 0 {
+        dist[vy][vx] = dist[uy][ux] + 1;
+        que.push_back((vx, vy));
       }
-      dist[y2][x2] = dist[y][x] + 1;
-      que.push_back((x2, y2));
     }
   }
-  dist[h-1][w-1]
+  dist[gy][gx]
 }
 
 fn main() {
@@ -75,19 +65,22 @@ fn main() {
     if w == 0 && h == 0 {
       break;
     }
-    let mut wall = vec![vec![Wall::new(); w]; h];
-    for i in 0..h-1 {
-      for j in 0..w-1 {
-        wall[i][j].right = read::<usize>() > 0;
+    let mut wall = vec![vec![(false, false); w]; h]; //右、下方向が壁かどうか
+    for y in 0..h-1 {
+      for x in 0..w-1 {
+        let v: usize = read();
+        wall[y][x].0 = v == 1;
       }
-      for j in 0..w {
-        wall[i][j].bottom = read::<usize>() > 0;
+      for x in 0..w {
+        let v: usize = read();
+        wall[y][x].1 = v == 1;
       }
     }
-    for j in 0..w-1 {
-      wall[h-1][j].right = read::<usize>() > 0;
+    for x in 0..w-1 {
+      let v: usize = read();
+      wall[h-1][x].0 = v == 1;
     }
-    let ans = bfs(h, w, &wall);
+    let ans = bfs(h, w, &wall, 0, 0, h-1, w-1);
     println!("{}", ans);
   }
 }
