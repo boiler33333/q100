3d72ebde628b16204decc9895da1fc548682c3fc
boiler <km.angstr3mp@gmail.com>
2021-09-12 12:48:25 +0900

    update

--- src/q100/034_045_knapsack_dp/039.rs
+++ src/q100/034_045_knapsack_dp/039.rs
@@ -1,26 +1,21 @@
 use proconio::input;
-use std::collections::HashMap;
 
 fn main() {
   input! {
     n: usize,
-    a: [i64; n], // a[0] ± ... ± a[n-2] = a[n-1]
+    a: [usize; n],
   }
-  let mut dp: Vec<HashMap<i64, usize>> = vec![HashMap::new(); n];
-  dp[0].insert(a[0], 1);
+  let mut dp: Vec<Vec<usize>> = vec![vec![0; 21]; n-1];
+  dp[0][a[0]] = 1;
   for i in 1..n-1 {
-    for j in 0..=20 {
-      if j + a[i] <= 20 {
-        let pv = *dp[i-1].get(&j).unwrap_or(&0);
-        let cv = *dp[i].get(&(j+a[i])).unwrap_or(&0);
-        dp[i].insert(j+a[i], cv + pv);
+    for j in 0..21 {
+      if j >= a[i] {
+        dp[i][j] += dp[i-1][j-a[i]];
       }
-      if j - a[i] >= 0 {
-        let pv = *dp[i-1].get(&j).unwrap_or(&0);
-        let cv = *dp[i].get(&(j-a[i])).unwrap_or(&0);
-        dp[i].insert(j-a[i], cv + pv);
+      if j + a[i] <= 20 {
+        dp[i][j] += dp[i-1][j+a[i]];
       }
     }
   }
-  println!("{}", dp[n-2].get(&a[n-1]).unwrap_or(&0));
+  println!("{}", dp[n-2][a[n-1]]);
 }
