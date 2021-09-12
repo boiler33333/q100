569d1ec82458ab57c9e26d2694ec89358bf22b09
boiler <km.angstr3mp@gmail.com>
2021-09-12 17:07:40 +0900

    update

--- src/q100/034_045_knapsack_dp/041.rs
+++ src/q100/034_045_knapsack_dp/041.rs
@@ -3,38 +3,34 @@ use std::cmp::max;
 
 fn main() {
   input! {
+    d: usize,
     n: usize,
-    m: usize,
-    t: [i64; n],
+    t: [i64; d],
+    abc: [(i64, i64, i64); n],
   }
-  let mut a = vec![0; m];
-  let mut b = vec![0; m];
-  let mut c = vec![0; m];
-  for j in 0..m {
-    input! { aj: i64, bj: i64, cj: i64 }
-    a[j] = aj;
-    b[j] = bj;
-    c[j] = cj;
-  }
-  let mut suit = vec![vec![false; m]; n];
-  for i in 0..n {
-    for j in 0..m {
-      suit[i][j] = a[j] <= t[i] && t[i] <= b[j];
+  let mut available = vec![vec![false; n]; d];
+  for i in 0..d {
+    for j in 0..n {
+      let (a, b, _) = abc[j];
+      available[i][j] = a <= t[i] && t[i] <= b;
     }
   }
-  let mut dp = vec![vec![0; m]; n];
-  for i in 0..n-1 {
-    for j in 0..m {
-      if !suit[i][j] {
+  let mut dp = vec![vec![0; n]; d];
+  for i in 0..d-1 {
+    for j in 0..n {
+      if !available[i][j] {
         continue;
       }
-      for k in 0..m {
-        if suit[i+1][k] {
-          dp[i+1][k] = max(dp[i+1][k], dp[i][j] + (c[j] - c[k]).abs());
+      let (_, _, cj) = abc[j];
+      for k in 0..n {
+        if !available[i+1][k] {
+          continue;
         }
+        let (_, _, ck) = abc[k];
+        dp[i+1][k] = max(dp[i+1][k], dp[i][j] + (cj - ck).abs());
       }
     }
   }
-  let ans = dp[n-1].iter().max().unwrap();
+  let ans = dp[d-1].iter().max().unwrap();
   println!("{}", ans);
 }
