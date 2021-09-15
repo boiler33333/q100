102fca42844f9cd4bbef83c40b1bd918301d9fab
boiler <km.angstr3mp@gmail.com>
2021-09-16 08:02:23 +0900

    update

--- src/q100/053_055_dp/054.rs
+++ src/q100/053_055_dp/054.rs
@@ -1,26 +1,5 @@
 use proconio::input;
-use std::cmp::Ordering::{ Less, Equal, Greater };
 use std::usize::MAX;
-
-trait BinarySearch<T> {
-  fn lower_bound(&self, x: &T) -> usize;
-}
-
-impl<T: Ord> BinarySearch<T> for [T] {
-  fn lower_bound(&self, x: &T) -> usize {
-    let mut left = 0;
-    let mut right = self.len();
-    while left < right {
-      let mid = (left + right) / 2;
-      let ord = self[mid].cmp(x);
-      match ord {
-        Less => left = mid + 1,
-        Equal | Greater => right = mid,
-      }
-    }
-    left
-  }
-}
 
 fn main() {
   input! {
@@ -28,12 +7,11 @@ fn main() {
     c: [usize; n],
   }
   let mut dp = vec![MAX; n+1];
-  for v in c {
-    let i = dp.lower_bound(&v);
-    dp[i] = v;
+  for i in 0..n {
+    let j = match dp.binary_search(&c[i]) { Ok(j) => j, Err(j) => j }; 
+    dp[j] = c[i];
   }
-  // lis: longest increasing sequence 
-  let lis = dp.iter().position(|&x| x == MAX).unwrap();
-  let ans = n - lis;
+  let m = dp.iter().position(|&x| x == MAX).unwrap();
+  let ans = n - m;
   println!("{}", ans);
 }
