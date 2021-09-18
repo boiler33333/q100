f6eccce9b087c349f72760ba4280279a8f27443c
boiler <km.angstr3mp@gmail.com>
2021-09-18 15:24:48 +0900

    update

--- src/q100/064_067_minimum_spanning_tree/065.rs
+++ src/q100/064_067_minimum_spanning_tree/065.rs
@@ -1,13 +1,13 @@
 use proconio::input;
 
 struct UnionFind {
-  par: Vec<Option<usize>>, //parent
-  siz: Vec<usize>,         //size
+  par: Vec<Option<usize>>, // parent
+  siz: Vec<usize>,         // size
 }
 
 impl UnionFind {
   fn new(n: usize) -> Self {
-    UnionFind{ par: vec![None; n], siz: vec![1; n] }
+    UnionFind{ par: vec![None; n+1], siz: vec![1; n+1] }
   }
 
   fn root(&self, x: usize) -> usize {
@@ -39,24 +39,47 @@ impl UnionFind {
 
 fn main() {
   input! {
-    n: usize, //都市の数
-    m: usize, //道路の数
-    mut k: usize, //本線を開催する年の個数
-    mut abc: [(usize, usize, usize); m], //都市aとbを結ぶ料金cの道路
+    n: usize, m: usize,
+    mut k: usize,
+    mut abc: [(usize, usize, usize); m],
   }
+  let ans = solve(n, k, &mut abc);
+  println!("{}", ans);
+}
+
+fn solve(n: usize, k: usize, abc: &mut Vec<(usize, usize, usize)>) -> usize {
   abc.sort_by(|x, y| x.2.cmp(&y.2));
-  let mut ans = 0;
   let mut uf = UnionFind::new(n);
-  for (a, b, c) in abc {
-    if k >= n {
+  let mut i = k;
+  let mut res = 0;
+  for &mut (a, b, c) in abc {
+    if i >= n {
       break;
     }
     if uf.is_same(a-1, b-1) {
       continue;
     }
-    ans += c;
     uf.unite(a-1, b-1);
-    k += 1;
+    i += 1;
+    res += c;
   }
-  println!("{}", ans);
+  res
+}
+
+#[test]
+fn test_solve_1() {
+  let n = 4;
+  let k = 1;
+  let mut abc = vec![(1,2,2),(2,3,9),(2,4,5)];
+  let got = solve(n, k, &mut abc);
+  assert_eq!(got, 16);
+}
+
+#[test]
+fn test_solve_2() {
+  let n = 5;
+  let k = 2;
+  let mut abc = vec![(1,2,5),(1,3,3),(2,3,4),(2,5,7),(3,4,6),(4,5,5)];
+  let got = solve(n, k, &mut abc);
+  assert_eq!(got, 12);
 }
