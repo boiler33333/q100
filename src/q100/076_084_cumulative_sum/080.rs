d204fb7105d79e5483e4cadce83580832dc117bc
boiler <km.angstr3mp@gmail.com>
2021-09-22 07:13:50 +0900

    update

--- src/q100/076_084_cumulative_sum/080.rs
+++ src/q100/076_084_cumulative_sum/080.rs
@@ -1,46 +1,63 @@
 use proconio::input;
 use std::cmp::max;
 
-fn cumulative_sum(h: usize, w: usize, graph: &mut Vec<Vec<usize>>) {
-  for y in 0..h {
-    for x in 1..w {
-      graph[y][x] += graph[y][x-1];
-    } 
-  }
-  for x in 0..w {
-    for y in 1..h {
-      graph[y][x] += graph[y-1][x];
-    } 
-  }
-}
-
-fn count(y1: usize, x1: usize, y2: usize, x2: usize, graph: &Vec<Vec<usize>>) -> usize {
-  graph[y2][x2] + graph[y1-1][x1-1] - graph[y2][x1-1] - graph[y1-1][x2]
-}
-
 fn main() {
   input! {
-    h: usize, w: usize, k: usize, v: usize,
+    h: usize,
+    w: usize,
+    k: usize,
+    v: usize,
     mut a: [[usize; w]; h],
   }
-  a.insert(0, vec![0; w]);
-  for y in 0..h+1 {
+  let ans = solve(h, w, k, v, &mut a);
+  println!("{}", ans);
+}
+
+fn cumulative_sum(h: usize, w: usize, table: &mut Vec<Vec<usize>>) {
+  for y in 0..h {
+    for x in 1..w {
+      table[y][x] += table[y][x-1];
+    }
+  }
+  for y in 1..h {
+    for x in 0..w {
+      table[y][x] += table[y-1][x];
+    }
+  }
+}
+
+fn calc((a, b, c, d): (usize, usize, usize, usize), table: &Vec<Vec<usize>>) -> usize {
+  table[c][d] + table[a][b] - table[c][b] - table[a][d]
+}
+
+fn solve(
+  h: usize,
+  w: usize,
+  k: usize,
+  v: usize,
+  a: &mut Vec<Vec<usize>>,
+) -> usize {
+  let h = h + 1;
+  let w = w + 1;
+  for y in 0..h-1 {
     a[y].insert(0, 0);
   }
-  cumulative_sum(h+1, w+1, &mut a);
-  let mut ans = 0;
-  for y2 in 1..h+1 {
-    for x2 in 1..w+1 {
-      for y1 in 1..=y2 {
-        for x1 in 1..=x2 {
-          let s = (y2-y1+1) * (x2-x1+1);
-          let cost = count(y1, x1, y2, x2, &a) + s * k;
+  a.insert(0, vec![0; w]);
+  cumulative_sum(h, w, a);
+  let mut ret = 0;
+  for y2 in 1..h {
+    for y1 in 0..y2 {
+      for x2 in 1..w {
+        for x1 in 0..x2 {
+          let s = (y2-y1) * (x2-x1);
+          let b = calc((y1, x1, y2, x2), &a);
+          let cost = b + s * k; 
           if cost <= v {
-            ans = max(ans, s);
+            ret = max(ret, s);
           }
         }
       }
     }
   }
-  println!("{}", ans);
+  ret
 }
