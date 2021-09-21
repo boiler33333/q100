17d406abf118b47cbe65ffde25fc5ff894ca6c4a
boiler <km.angstr3mp@gmail.com>
2021-09-22 08:26:54 +0900

    update

--- src/q100/076_084_cumulative_sum/081.rs
+++ src/q100/076_084_cumulative_sum/081.rs
@@ -1,23 +1,18 @@
 use proconio::input;
-use std::cmp::max;
 
 fn main() {
   input! {
     n: usize,
     ab: [(usize, usize); n],
   }
-  let m = 1_000_001;
-  let mut acc: Vec<i64> = vec![0; m+1];
+  let mut acc = vec![0; 1_000_002];
   for (a, b) in ab {
     acc[a] += 1;
     acc[b+1] -= 1;
   }
-  for i in 0..m {
-    acc[i+1] += acc[i];
+  for i in 1..1_000_002 {
+    acc[i] += acc[i-1];
   }
-  let mut ans = 0;
-  for i in 0..=m {
-    ans = max(ans, acc[i]);
-  }
+  let ans = acc.iter().max().unwrap();
   println!("{}", ans);
 }
