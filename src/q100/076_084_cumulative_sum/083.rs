61a7e06a87833853091aecf575834c2508e6e5ca
boiler <km.angstr3mp@gmail.com>
2021-09-23 07:12:48 +0900

    update

--- src/q100/076_084_cumulative_sum/083.rs
+++ src/q100/076_084_cumulative_sum/083.rs
@@ -3,29 +3,29 @@ use std::cmp::min;
 
 fn main() {
   input! {
-    n: usize,      //都市の数
-    m: usize,      //日数
-    p: [usize; m], //行き先
-    abc: [(i64, i64, i64); n-1], //紙での運賃, カードでの運賃、カード代
+    n: usize,
+    m: usize,
+    p: [usize; m],
+    abc: [(i64, i64, i64); n-1],
   }
-  let mut acc: Vec<i64> = vec![0; n];
-  for i in 0..m-1 {
-    if p[i+1] > p[i] {
-      acc[p[i]-1] += 1;
-      acc[p[i+1]-1] -= 1;
-    } else {
-      acc[p[i+1]-1] += 1;
+  let mut acc = vec![0i64; n];
+  for i in 1..m {
+    if p[i] > p[i-1] {
+      acc[p[i-1]-1] += 1;
       acc[p[i]-1] -= 1;
+    } else {
+      acc[p[i]-1] += 1;
+      acc[p[i-1]-1] -= 1;
     }
   }
-  for i in 0..n-1 {
-    acc[i+1] += acc[i];
+  for i in 1..n {
+    acc[i] += acc[i-1];
   }
   let mut ans = 0;
   for i in 0..n-1 {
     let (a, b, c) = abc[i];
-    let paper = a * acc[i];
-    let card = b * acc[i] + c;
+    let paper = acc[i] * a;
+    let card = acc[i] * b + c;
     ans += min(paper, card);
   }
   println!("{}", ans);
