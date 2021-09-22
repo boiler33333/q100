28caa4fa15e8f81c0741f64c9a1fe948fe437ac8
boiler <km.angstr3mp@gmail.com>
2021-09-22 09:02:12 +0900

    update

--- src/q100/076_084_cumulative_sum/082.rs
+++ src/q100/076_084_cumulative_sum/082.rs
@@ -12,32 +12,42 @@ fn read<T: FromStr>() -> T {
   s.parse().ok().expect("failed parsing")
 }
 
-fn seconds(s: String) -> usize {
-  let b: Vec<u8> = s.bytes().collect();
-  let hh: usize = (10 * (b[0] - 48) + (b[1] - 48)).into();
-  let mm: usize = (10 * (b[3] - 48) + (b[4] - 48)).into();
-  let ss: usize = (10 * (b[6] - 48) + (b[7] - 48)).into();
+fn second(s: &str) -> usize {
+  let b: Vec<usize> = s.bytes().map(|b| (b - 48) as usize).collect();
+  let hh = 10 * b[0] + b[1];
+  let mm = 10 * b[3] + b[4];
+  let ss = 10 * b[6] + b[7];
   3600 * hh + 60 * mm + ss
+}
+
+fn solve(n: usize, st: &[String], et: &[String]) -> i64 {
+  let m = 24 * 3600;
+  let mut acc = vec![0; m];
+  for i in 0..n {
+    let a = second(&st[i]);
+    let b = second(&et[i]);
+    acc[a] += 1;
+    acc[b] -= 1;
+  }
+  for i in 1..m {
+    acc[i] += acc[i-1];
+  }
+  *acc.iter().max().unwrap()
 }
 
 fn main() {
   loop {
-    let n = read::<usize>();
+    let n: usize = read();
     if n == 0 {
       break;
     }
-    let m = 3600 * 24;
-    let mut acc = vec![0; m+1];
+    let mut st = vec![];
+    let mut et = vec![];
     for _ in 0..n {
-      let st = seconds(read::<String>());
-      let et = seconds(read::<String>());
-      acc[st] += 1;
-      acc[et] -= 1;
+      st.push(read::<String>());
+      et.push(read::<String>());
     }
-    for i in 0..m {
-      acc[i+1] += acc[i];
-    }
-    let ans = acc.iter().max().unwrap();
+    let ans = solve(n, &st, &et);
     println!("{}", ans);
   }
 }
