24f8373d0d96715bd59f6aaa5a835774a56406e5
boiler <km.angstr3mp@gmail.com>
2021-09-23 07:36:01 +0900

    update

--- src/q100/076_084_cumulative_sum/084.rs
+++ src/q100/076_084_cumulative_sum/084.rs
@@ -2,12 +2,17 @@ use proconio::input;
 
 fn main() {
   input! {
-    n: usize, //正三角形の一辺に並んでいる釘の本
-    m: usize, //輪ゴムの数
-    abx: [(usize, usize, usize); m], //3本の釘 (Ai, Bi), (Ai + Xi, Bi), (Ai + Xi, Bi + Xi)
+    n: usize,
+    m: usize,
+    abx: [(usize, usize, usize); m],
   }
+  let ans = solve(n, m, &abx);
+  println!("{}", ans);
+}
+
+fn solve(n: usize, _: usize, abx: &[(usize, usize, usize)]) -> usize {
   let mut acc = vec![vec![0; n+3]; n+3];
-  for (a, b, x) in abx {
+  for &(a, b, x) in abx {
     acc[a][b] += 1;
     acc[a][b+1] -= 1;
     acc[a+x+1][b] -= 1;
@@ -30,13 +35,22 @@ fn main() {
       acc[a][b] += acc[a-1][b-1];
     }
   }
-  let mut ans = 0;
+  let mut ret = 0;
   for a in 1..=n {
     for b in 1..=a {
       if acc[a][b] > 0 {
-        ans += 1;
+        ret += 1;
       }
     }
   }
-  println!("{}", ans);
+  ret
+}
+
+#[test]
+fn test_solve() {
+  let n = 5;
+  let m = 2;
+  let abx = vec![(2,2,1),(2,1,3)];
+  let got = solve(n, m, &abx);
+  assert_eq!(got, 12);
 }
