4337f5efb67949a3784182912046c50a6f0b3c0c
boiler <km.angstr3mp@gmail.com>
2021-09-26 10:32:41 +0900

    update

--- src/q100/092_094_implemention_problem/094.rs
+++ src/q100/092_094_implemention_problem/094.rs
@@ -1,5 +1,4 @@
-use std::cmp::max;
-use std::cmp::min;
+use std::cmp::{max, min};
 use std::io::*;
 use std::str::FromStr;
 
@@ -14,39 +13,6 @@ fn read<T: FromStr>() -> T {
   s.parse().ok().expect("failed parsing")
 }
 
-type Cake = (usize, usize);
-
-fn cut(cake: Cake, s: usize) -> (Cake, Cake) {
-  let (w, d) = cake;
-  let s = s % (w + d);
-  if s < w {
-    let w1 = min(s, w - s);
-    let w2 = max(s, w - s);
-    ((w1, d), (w2, d))
-  } else {
-    let d1 = min(s - w, w + d - s);
-    let d2 = max(s - w, w + d - s);
-    ((w, d1), (w, d2))
-  }
-}
-
-fn solve(w: usize, d: usize, ps: &Vec<(usize, usize)>) {
-  let mut cake = (w, d);
-  let mut cakes = vec![cake];
-  for &(p, s) in ps {
-    cake = cakes[p];
-    cakes.remove(p);
-    let (cake1, cake2) = cut(cake, s);
-    cakes.push(cake1);
-    cakes.push(cake2);
-  } 
-  let mut area: Vec<usize> = cakes.iter().map(|&(w, d)| w * d).collect();
-  area.sort();
-  let area: Vec<String> = area.iter().map(|x| x.to_string()).collect();
-  let ans = area.join(" ");
-  println!("{}", ans);
-}
-
 fn main() {
   loop {
     let n: usize = read();
@@ -55,12 +21,63 @@ fn main() {
     if n == 0 && w == 0 && d == 0 {
       break;
     }
-    let mut ps: Vec<(usize, usize)> = Vec::new();
+    let mut ps = vec![];
     for _ in 0..n {
       let p: usize = read();
       let s: usize = read();
-      ps.push((p-1, s));
+      ps.push((p, s));
     }
-    solve(w, d, &ps);
+    let vec = solve(w, d, &ps);
+    let vec = vec.iter().map(|v| v.to_string()).collect::<Vec<String>>();
+    let ans = vec.join(" ");
+    println!("{}", ans);
   }
+}
+
+fn solve(w: usize, d: usize, ps: &[(usize, usize)]) -> Vec<usize> {
+  let mut pieces = vec![(w, d)];
+  for &(p, s) in ps {
+    let piece = pieces[p-1];
+    pieces.remove(p-1);
+    let (small, large) = cut(piece, s);
+    pieces.push(small);
+    pieces.push(large);
+  }
+  let mut area: Vec<usize> = pieces.iter().map(|&(w, d)| w * d).collect();
+  area.sort();
+  area
+}
+
+fn cut((w, d): (usize, usize), s: usize) -> ((usize, usize), (usize, usize)) {
+  let s =  s % (w + d);
+  if s < w {
+    let w1 = s;
+    let w2 = w - s;
+    ((min(w1, w2), d), (max(w1, w2), d))
+  } else {
+    let h1 = s - w;
+    let h2 = w + d - s;
+    ((w, min(h1, h2)), (w, max(h1, h2)))
+  }
+}
+
+#[test]
+fn test_solve_1() {
+  let got = solve(5, 6, &[(1,18),(2,19),(1,2)]);
+  let want = vec![4,4,6,16];
+  assert_eq!(got, want); 
+}
+
+#[test]
+fn test_solve_2() {
+  let got = solve(4, 1, &[(1,1),(2,1),(3,1)]);
+  let want = vec![1,1,1,1];
+  assert_eq!(got, want); 
+}
+
+#[test]
+fn test_solve_3() {
+  let got = solve(2, 5, &[]);
+  let want = vec![10];
+  assert_eq!(got, want); 
 }
