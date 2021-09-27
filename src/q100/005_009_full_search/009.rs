5fd55b89790d3d3555291202c8f8c309be927a6b
boiler <km.angstr3mp@gmail.com>
2021-09-28 08:47:49 +0900

    update

--- src/q100/005_009_full_search/009.rs
+++ src/q100/005_009_full_search/009.rs
@@ -3,34 +3,26 @@ use std::collections::HashSet;
 
 fn main() {
   input! {
-    m: usize,
-    p: [(i64, i64); m],
     n: usize,
-    q: [(i64, i64); n],
-  }
-  let (dx, dy) = solve(&p, &q);
-  println!("{} {}", dx, dy);
-}
-
-fn solve(
-  p: &Vec<(i64, i64)>,
-  q: &Vec<(i64, i64)>,
-) -> (i64, i64) {
-  let hs: HashSet::<(i64, i64)> = q.iter().cloned().collect();
-  for &(px, py) in p {
-    for &(qx, qy) in q {
-      let dx = qx - px;
-      let dy = qy - py;
-      let mut find = true;
-      for &(x, y) in p {
-        if !hs.contains(&(x + dx, y + dy)) {
-          find = false;
+    p: [(i64, i64); n],
+    m: usize,
+    q: [(i64, i64); m],
+  };
+  let hs: HashSet<(i64, i64)> = q.iter().cloned().collect();
+  for &(x1, y1) in &p {
+    for &(x2, y2) in &q {
+      let dx = x2 - x1;
+      let dy = y2 - y1;
+      let mut ok = true;
+      for &(x, y) in &p {
+        if !hs.contains(&(x+dx, y+dy)) {
+          ok = false;
         }
       }
-      if find {
-        return (dx, dy);
+      if ok {
+        println!("{} {}", dx, dy);
+        return;
       }
     }
   }
-  (0, 0)
 }
