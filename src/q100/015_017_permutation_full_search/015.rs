9b7d7dbad02f8f2e41711620360e04889a34aba3
boiler <km.angstr3mp@gmail.com>
2021-09-29 08:17:44 +0900

    update

--- src/q100/015_017_permutation_full_search/015.rs
+++ src/q100/015_017_permutation_full_search/015.rs
@@ -1,30 +1,26 @@
 use proconio::input;
 use itertools::Itertools;
 
-fn dist((x1, y1): (f64, f64), (x2, y2): (f64, f64)) -> f64 {
-  ((x2 - x1).powi(2) + (y2 - y1).powi(2)).sqrt()
-}
-
-fn factorial(k: usize) -> usize {
-  (2..=k).fold(1, |sum, i| sum * i)
-}
-
 fn main() {
   input! {
     n: usize,
     xy: [(f64, f64); n],
   }
+  let mut table = vec![vec![0.0; n]; n];
+  for v in 0..n {
+    for u in 0..n {
+      let (x2, y2) = xy[v];
+      let (x1, y1) = xy[u];
+      table[u][v] = ((x2-x1).powi(2)+(y2-y1).powi(2)).sqrt();
+    }
+  }
   let mut sum = 0.0;
   for p in (0..n).permutations(n) {
-    let mut d = 0.0;
-    for i in 0..n-1 {
-      let u = p[i];
-      let v = p[i+1];
-      d += dist(xy[u], xy[v]);
+    for i in 1..n {
+      sum += table[p[i]][p[i-1]];
     }
-    sum += d;
   }
-  let m = factorial(n) as f64;
-  let ans = sum / m;
+  let fac = (1..=n).fold(1, |sum, i| sum * i) as f64;
+  let ans = sum / fac;
   println!("{:.10}", ans);
 }
