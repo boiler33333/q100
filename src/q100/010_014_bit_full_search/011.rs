19eaa1c26f7f047f21212a132aedfd1c2a269ace
boiler <km.angstr3mp@gmail.com>
2021-09-29 07:03:34 +0900

    update

--- src/q100/010_014_bit_full_search/011.rs
+++ src/q100/010_014_bit_full_search/011.rs
@@ -2,29 +2,29 @@ use proconio::input;
 
 fn main() {
   input! { n: usize, m: usize }
-  let mut switches: Vec<Vec<i64>> = Vec::new();
+  let mut s = vec![];
   for _ in 0..m {
-    input! { k: usize, s: [i64; k] }
-    switches.push(s)
+    input! { k: usize, si: [usize; k] }
+    s.push(si);
   }
   input! { p: [usize; m] }
   let mut ans = 0;
-  for status in 0..1<<n {
+  for state in 0..1<<n {
     let mut ok = true;
-    for i in 0..m {
+    for j in 0..m {
       let mut cnt = 0;
-      for &s in &switches[i] {
-        if status & 1 << s-1 != 0 {
+      for &i in &s[j] {
+        if state & 1 << (i-1) > 0 {
           cnt += 1;
         }
       }
-      if cnt % 2 != p[i] {
+      if cnt % 2 != p[j] {
         ok = false;
       }
     }
     if ok {
       ans += 1;
-    }
+    } 
   }
   println!("{}", ans);
 }
