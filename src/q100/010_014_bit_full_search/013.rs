d459f15e4b9d2aa5f0dbad9a32d19fa4cd96c421
boiler <km.angstr3mp@gmail.com>
2021-09-29 07:29:53 +0900

    update

--- src/q100/010_014_bit_full_search/013.rs
+++ src/q100/010_014_bit_full_search/013.rs
@@ -7,27 +7,26 @@ fn main() {
     a: [[usize; c]; r],
   }
   let mut ans = 0;
-  for status in 0..1<<r {
+  for state in 0..1<<r {
     let mut table = a.clone();
     for y in 0..r {
-      if status & 1 << y != 0 {
+      if state & 1 << y > 0 {
         for x in 0..c {
           table[y][x] = 1 - table[y][x];
         }
       }
     }
-    let mut cnt = 0;
+    let mut sum = 0;
     for x in 0..c {
-      let mut heads = 0;
+      let mut cnt = 0;
       for y in 0..r {
-        if table[y][x] == 0 {
-          heads += 1;
+        if table[y][x] > 0 {
+          cnt += 1;
         }
       }
-      let tails = r - heads;
-      cnt += max(heads, tails);
+      sum += max(cnt, r - cnt);
     }
-    ans = max(ans ,cnt);
+    ans = max(ans, sum);
   }
   println!("{}", ans);
 }
