90f5a5c3a3b3527e08bc9562df134bcaf4ed82a8
boiler <km.angstr3mp@gmail.com>
2021-09-29 06:46:18 +0900

    update

--- src/q100/010_014_bit_full_search/010.rs
+++ src/q100/010_014_bit_full_search/010.rs
@@ -14,48 +14,30 @@ fn read<T: FromStr>() -> T {
 
 fn main() {
   let n: usize = read();
-  let mut a: Vec<usize> = vec![0; n];
+  let mut a = vec![0; n];
   for i in 0..n {
     a[i] = read();
   }
-  let q: usize = read();
-  let mut m: Vec<usize> = vec![0; q];
-  for j in 0..q {
-    m[j] = read();
+  let m: usize = read();
+  let mut b = vec![0; m];
+  for j in 0..m {
+    b[j] = read();
   }
-  let result = solve(n, &a, q, &m);
-  for ok in result {
-    println!("{}", ok);
-  }
-}
-
-fn solve(n: usize, a: &[usize], q: usize, m: &[usize]) -> Vec<String> {
-  let mut res = vec![String::from("no"); q];
-  for status in 0..1<<n {
+  let mut ans = vec![false; m];
+  for state in 0..1<< n {
     let mut sum = 0;
     for i in 0..n {
-      if status & 1 << i != 0 {
+      if state & 1 << i > 0 {
         sum += a[i];
       }
     }
-    for j in 0..q {
-      if m[j] == sum {
-        res[j] = String::from("yes");
+    for j in 0..m {
+      if b[j] == sum {
+        ans[j] = true;
       }
     }
   }
-  res
-}
-
-#[test]
-fn test_solve_1() {
-  let n = 5;
-  let s = vec![1,5,7,10,21];
-  let q = 4;
-  let t = vec![2,4,17,8];
-  let got = solve(n, &s, q, &t);
-  let want = vec!["no", "no", "yes", "yes"];
-  for j in 0..q {
-    assert_eq!(got[j], want[j]);
+  for j in 0..m {
+    println!("{}", if ans[j] { "yes" } else { "no" });
   }
 }
