95c822e105135cec87b7b35141db81f0db7f2cc7
boiler <km.angstr3mp@gmail.com>
2021-10-01 09:51:22 +0900

    update

--- src/q100/028_033_breadth_first_search/029.rs
+++ src/q100/028_033_breadth_first_search/029.rs
@@ -1,37 +1,31 @@
 use proconio::input;
 use std::collections::VecDeque;
+use std::usize::MAX;
 
 fn bfs(
   h: usize,
   w: usize,
   table: &Vec<Vec<char>>,
-  sy: usize,
-  sx: usize,
-  gy: usize,
-  gx: usize,
-) -> Option<usize> {
-  let mut dist = vec![vec![None; w]; h];
-  let mut que: VecDeque<(usize, usize)> = VecDeque::new();
-  dist[sy][sx] = Some(0);
+  (sx, sy): (usize, usize),
+  (gx, gy): (usize, usize),
+) -> usize {
+  let mut dist = vec![vec![MAX; w]; h];
+  let mut que = VecDeque::new();
+  dist[sy][sx] = 0;
   que.push_back((sx, sy));
   while let Some((ux, uy)) = que.pop_front() {
     for i in 0..4 {
-      if uy == 0 && i == 0 || uy == h-1 && i == 2 {
-        continue;
-      }
-      if ux == 0 && i == 3 || ux == w-1 && i == 1 {
-        continue;
-      }
       let (vx, vy) = match i {
         0 => (ux + 0, uy - 1),
         1 => (ux + 1, uy + 0),
         2 => (ux + 0, uy + 1),
-        _ => (ux - 1, uy + 0),
+        3 => (ux - 1, uy + 0),
+        _ => unreachable!(),
       };
-      if table[vy][vx] == '#' || dist[vy][vx] != None {
+      if table[vy][vx] == '#' || dist[vy][vx] < MAX {
         continue;
       }
-      dist[vy][vx] = dist[uy][ux].and_then(|d| Some(d+1));
+      dist[vy][vx] = dist[uy][ux] + 1;
       que.push_back((vx, vy));
     }
   }
@@ -40,12 +34,12 @@ fn bfs(
 
 fn main() {
   input! {
-    r: usize, c: usize,
+    h: usize, w: usize,
     sy: usize, sx: usize,
     gy: usize, gx: usize,
-    table: [String; r],
+    c: [String; h],
   }
-  let table: Vec<Vec<char>> = table.iter().map(|s| s.chars().collect()).collect();
-  let ans = bfs(r, c, &table, sy-1, sx-1, gy-1, gx-1);
-  println!("{}", ans.unwrap());
+  let c = c.iter().map(|s| s.chars().collect()).collect();
+  let ans = bfs(h, w, &c, (sx-1, sy-1), (gx-1, gy-1));
+  println!("{}", ans);
 }
