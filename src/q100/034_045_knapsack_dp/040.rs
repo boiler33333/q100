6c18bf88e99e1ce7f22dcc9fe498d1fca3d6bd6f
boiler <km.angstr3mp@gmail.com>
2021-10-02 18:19:19 +0900

    update

--- src/q100/034_045_knapsack_dp/040.rs
+++ src/q100/034_045_knapsack_dp/040.rs
@@ -1,4 +1,5 @@
 use proconio::input;
+use std::collections::HashMap;
 
 fn main() {
   input! {
@@ -6,34 +7,36 @@ fn main() {
     k: usize,
     ab: [(usize, usize); k],
   }
-  let mut pasta = vec![0; n];
+  let mut hm = HashMap::new();
   for &(a, b) in &ab {
-    pasta[a-1] = b;
+    hm.insert(a-1, b);
   }
   // i日目、i-1日目のパスタ、i-2日目のパスタ
   let mut dp = vec![vec![vec![0; 4]; 4]; n+1];
   dp[0][0][0] = 1;
   for i in 0..n {
-    for b in 1..4 {
-      if pasta[i] > 0 && pasta[i] != b {
-        continue;
+    for b0 in 1..=3 {
+      if let Some(&b) = hm.get(&i) {
+        if b0 != b {
+          continue;
+        }
       }
-      for b1 in 0..4 {
-        for b2 in 0..4 {
-          if b == b1 && b1 == b2 {
+      for b1 in 0..=3 {
+        for b2 in 0..=3 {
+          if b0 == b1 && b1 == b2 {
             continue;
           }
-          dp[i+1][b][b1] += dp[i][b1][b2];
-          dp[i+1][b][b1] %= 10000;
+          dp[i+1][b0][b1] += dp[i][b1][b2];
+          dp[i+1][b0][b1] %= 10_000;
         }
       }
     }
   }
   let mut ans = 0;
-  for i in 1..4 {
-    for j in 1..4 {
-      ans += dp[n][i][j];
-      ans %= 10000;
+  for b1 in 1..=3 {
+    for b2 in 1..=3 {
+      ans += dp[n][b1][b2];
+      ans %= 10_000;
     }
   }
   println!("{}", ans);
