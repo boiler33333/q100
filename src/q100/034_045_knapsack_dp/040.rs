a616997bf3d0f5ea014dd4669d709592e1822ceb
boiler <km.angstr3mp@gmail.com>
2021-10-02 18:23:59 +0900

    update

--- src/q100/034_045_knapsack_dp/040.rs
+++ src/q100/034_045_knapsack_dp/040.rs
@@ -9,12 +9,12 @@ fn main() {
   }
   let mut hm = HashMap::new();
   for &(a, b) in &ab {
-    hm.insert(a-1, b);
+    hm.insert(a, b);
   }
-  // i日目、i-1日目のパスタ、i-2日目のパスタ
+  // i日目、i日目のパスタ、i-1日目のパスタ
   let mut dp = vec![vec![vec![0; 4]; 4]; n+1];
   dp[0][0][0] = 1;
-  for i in 0..n {
+  for i in 1..=n {
     for b0 in 1..=3 {
       if let Some(&b) = hm.get(&i) {
         if b0 != b {
@@ -26,8 +26,8 @@ fn main() {
           if b0 == b1 && b1 == b2 {
             continue;
           }
-          dp[i+1][b0][b1] += dp[i][b1][b2];
-          dp[i+1][b0][b1] %= 10_000;
+          dp[i][b0][b1] += dp[i-1][b1][b2];
+          dp[i][b0][b1] %= 10_000;
         }
       }
     }
