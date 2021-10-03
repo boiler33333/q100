8c5c4baae698e36721b1cea42ea36e9c6e256afe
boiler <km.angstr3mp@gmail.com>
2021-10-03 09:25:57 +0900

    update

--- src/q100/034_045_knapsack_dp/041.rs
+++ src/q100/034_045_knapsack_dp/041.rs
@@ -3,34 +3,27 @@ use std::cmp::max;
 
 fn main() {
   input! {
-    d: usize,
-    n: usize,
-    t: [i64; d],
-    abc: [(i64, i64, i64); n],
+    d: usize,      //服の計画を立てる日数
+    n: usize,      //服の種類の数
+    t: [usize; d], //i日目の最高気温
+    abc: [(usize, usize, i64); n], //服
   }
-  let mut available = vec![vec![false; n]; d];
+  let mut clothes = vec![vec![]; d]; //i日目に利用できる服
   for i in 0..d {
     for j in 0..n {
-      let (a, b, _) = abc[j];
-      available[i][j] = a <= t[i] && t[i] <= b;
+      let &(a, b , c) = &abc[j];
+      if a <= t[i] && t[i] <= b {
+        clothes[i].push((j, c));
+      }
     }
   }
-  let mut dp = vec![vec![0; n]; d];
+  let mut dp = vec![vec![0; n+1]; d+1];
   for i in 0..d-1 {
-    for j in 0..n {
-      if !available[i][j] {
-        continue;
-      }
-      let (_, _, cj) = abc[j];
-      for k in 0..n {
-        if !available[i+1][k] {
-          continue;
-        }
-        let (_, _, ck) = abc[k];
-        dp[i+1][k] = max(dp[i+1][k], dp[i][j] + (cj - ck).abs());
+    for &(j0, c0) in &clothes[i] {
+      for &(j1, c1) in &clothes[i+1] {
+        dp[i+1][j1] = max(dp[i+1][j1], dp[i][j0] + (c1 - c0).abs());
       }
     }
   }
-  let ans = dp[d-1].iter().max().unwrap();
-  println!("{}", ans);
+  println!("{}", dp[d-1].iter().max().unwrap());
 }
