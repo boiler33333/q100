790e1245a1d2a74c488b3fa0a027b8e84ace69b6
boiler <km.angstr3mp@gmail.com>
2021-10-03 11:26:51 +0900

    update

--- src/q100/034_045_knapsack_dp/044.rs
+++ src/q100/034_045_knapsack_dp/044.rs
@@ -14,31 +14,27 @@ fn read<T: FromStr>() -> T {
   s.parse().ok().expect("failed parsing")
 }
 
-fn tetrahedron() -> Vec<usize> {
-  let mut a = vec![];
-  let mut v = 1;
+fn setup() -> (Vec<usize>, Vec<usize>) {
   let mut n = 1;
-  while v < 1_000_000 {
-    v = n * (n+1) * (n+2) / 6;
-    a.push(v);
-    n += 1;
+  let mut i = 1;
+  let mut xs = vec![];
+  while n < 1_000_000 {
+    n = i*(i+1)*(i+2)/6;
+    xs.push(n);
+    i += 1;
   }
-  a
-}
-
-fn gen_dp(tetrahedron: &Vec<usize>) -> (Vec<usize>, Vec<usize>) {
   let mut dp1 = vec![MAX; 1_000_000];
   let mut dp2 = vec![MAX; 1_000_000];
   dp1[0] = 0;
   dp2[0] = 0;
-  for &v in tetrahedron {
+  for x in xs {
     for n in 1..1_000_000 {
-      if n < v {
+      if n < x {
         continue;
       }
-      dp1[n] = min(dp1[n], dp1[n-v]+1);
-      if v % 2 == 1 {
-        dp2[n] = min(dp2[n], dp2[n-v]+1);
+      dp1[n] = min(dp1[n], dp1[n-x] + 1);
+      if x % 2 == 1 {
+        dp2[n] = min(dp2[n], dp2[n-x] + 1);
       }
     }
   }
@@ -46,25 +42,12 @@ fn gen_dp(tetrahedron: &Vec<usize>) -> (Vec<usize>, Vec<usize>) {
 }
 
 fn main() {
-  let a = tetrahedron();
-  let (dp1, dp2) = gen_dp(&a);
+  let (dp1, dp2) = setup();
   loop {
     let n: usize = read();
     if n == 0 {
       break;
     }
     println!("{} {}", dp1[n], dp2[n]);
-  }
-}
-
-#[test]
-fn test_solve() {
-  let arg = vec![40,14,5,165,120,103,106,139];
-  let a = tetrahedron();
-  let (dp1, dp2) = gen_dp(&a);
-  let want = vec![(2,6),(2,14),(2,5),(1,1),(1,18),(5,35),(4,4),(3,37)];
-  for (i, &n) in arg.iter().enumerate() {
-    assert_eq!(dp1[n], want[i].0);
-    assert_eq!(dp2[n], want[i].1);
   }
 }
