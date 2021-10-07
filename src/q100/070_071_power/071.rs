8b23796f8ba5d920c049c8feba356fc17088c775
boiler <km.angstr3mp@gmail.com>
2021-10-08 07:29:24 +0900

    update

--- src/q100/070_071_power/071.rs
+++ src/q100/070_071_power/071.rs
@@ -8,17 +8,19 @@ fn pow(x: i64, n: i64) -> i64 {
   } else if n % 2 == 0 {
     pow(x * x % MOD, n/2)
   } else {
-    x * pow(x, n-1) % MOD 
+    x * pow(x, n-1) % MOD
   }
 }
 
 fn main() {
   input! {
-    n: usize,
-    mut q: usize,
+    n: usize, mut q: usize,
     a: [i64; n],
     mut c: [usize; q],
   }
+  c.insert(0, 1);
+  c.push(1);
+  q = c.len();
   let mut acc = vec![0; n];
   for i in 1..n {
     acc[i] = pow(a[i-1], a[i]);
@@ -26,14 +28,12 @@ fn main() {
   for i in 1..n {
     acc[i] += acc[i-1];
   }
-  c.insert(0, 1);
-  c.push(1);
-  q = c.len();
   let mut ans = 0;
   for i in 1..q {
-    let p1 = acc[c[i-1]-1];
-    let p2 = acc[c[i]-1];
-    ans += (p2 - p1).abs() % MOD;
+    let x2 = acc[c[i]-1];
+    let x1 = acc[c[i-1]-1];
+    ans += (x2 - x1).abs();
+    ans %= MOD;
   }
-  println!("{}", ans % MOD);
+  println!("{}", ans);
 }
