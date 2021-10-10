e7081ac1718f7f7ec5c05c915963d9373fcbc26a
boiler <km.angstr3mp@gmail.com>
2021-10-10 10:15:40 +0900

    update

--- src/q100/076_084_cumulative_sum/080.rs
+++ src/q100/076_084_cumulative_sum/080.rs
@@ -1,18 +1,6 @@
 use proconio::input;
 use std::cmp::max;
 
-fn main() {
-  input! {
-    h: usize,
-    w: usize,
-    k: usize,
-    v: usize,
-    mut a: [[usize; w]; h],
-  }
-  let ans = solve(h, w, k, v, &mut a);
-  println!("{}", ans);
-}
-
 fn cumulative_sum(h: usize, w: usize, table: &mut Vec<Vec<usize>>) {
   for y in 0..h {
     for x in 1..w {
@@ -26,38 +14,40 @@ fn cumulative_sum(h: usize, w: usize, table: &mut Vec<Vec<usize>>) {
   }
 }
 
-fn calc((a, b, c, d): (usize, usize, usize, usize), table: &Vec<Vec<usize>>) -> usize {
-  table[c][d] + table[a][b] - table[c][b] - table[a][d]
+fn calc(a: usize, b: usize, c: usize, d: usize, table: &Vec<Vec<usize>>) -> usize {
+  let a = a - 1;
+  let b = b - 1;
+  table[c][d] + table[a][b] - table[a][d] - table[c][b]
 }
 
-fn solve(
-  h: usize,
-  w: usize,
-  k: usize,
-  v: usize,
-  a: &mut Vec<Vec<usize>>,
-) -> usize {
-  let h = h + 1;
-  let w = w + 1;
-  for y in 0..h-1 {
-    a[y].insert(0, 0);
+fn main() {
+  input! {
+    h: usize,
+    w: usize,
+    k: usize,
+    v: usize,
+    a: [[usize; w]; h],
   }
-  a.insert(0, vec![0; w]);
-  cumulative_sum(h, w, a);
-  let mut ret = 0;
-  for y2 in 1..h {
-    for y1 in 0..y2 {
-      for x2 in 1..w {
-        for x1 in 0..x2 {
-          let s = (y2-y1) * (x2-x1);
-          let b = calc((y1, x1, y2, x2), &a);
-          let cost = b + s * k; 
-          if cost <= v {
-            ret = max(ret, s);
+  let mut table = vec![vec![0; w+1]; h+1];
+  for y in 0..h {
+    for x in 0..w {
+      table[y+1][x+1] = a[y][x];
+    }
+  }
+  cumulative_sum(h+1, w+1, &mut table);
+  let mut ans = 0;
+  for y2 in 1..=h {
+    for y1 in 1..=y2 {
+      for x2 in 1..=w {
+        for x1 in 1..=x2 {
+          let s = (y2 - y1 + 1) * (x2 - x1 + 1);
+          let a = calc(y1, x1, y2, x2, &table);
+          if a + s * k <= v {
+            ans = max(ans, s);
           }
         }
       }
     }
   }
-  ret
+  println!("{}", ans);
 }
